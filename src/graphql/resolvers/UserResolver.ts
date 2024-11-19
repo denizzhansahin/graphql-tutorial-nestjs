331d8a32246a0cecbd4fdbca1c5cdf841a463b9e
Denizhan Şahin <denizhansahin@yaani.com>
2024-11-19 12:42:35 +0300

    first commit

--- src/graphql/resolvers/UserResolver.ts
+++ src/graphql/resolvers/UserResolver.ts
@@ -1,10 +1,15 @@
 //user modeli için çözümleyici
 
-import { Resolver ,Query, Args, Int, ResolveField, Parent} from "@nestjs/graphql";
+import { Resolver ,Query, Args, Int, ResolveField, Parent,Mutation} from "@nestjs/graphql";
 import { User } from "../models/User";
 import { mockUsers } from "src/_mocks_/mockUsers";
 import { UserSetting } from "../models/UserSetting";
 import { mockUsersSettings } from "src/_mocks_/mockUserSettings";
+
+
+
+export let incrementalId = 3
+
 
 @Resolver(of=>User)
 export class UserResolver {
@@ -22,5 +27,15 @@ export class UserResolver {
     getUserSettings(@Parent() user:User){
         console.log(user)
         return mockUsersSettings.find((setting)=>setting.userId===user.id)
+    }
+
+    @Mutation((returns)=>User)
+    createUser(
+        @Args('username') username : string,
+        @Args('displayName', {nullable:true}) displayName:string
+    ) {
+        const newUser = {username,displayName,id:++incrementalId}
+        mockUsers.push(newUser)
+        return newUser
     }
 }
